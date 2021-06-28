83d6b1d153f503bc53b7f229c6173979c7f9745f
RamirezF <francisco.ramirez@tecsup.edu.pe>
2021-06-28 14:21:24 -0500

    InvoiceDetail creado

---
+++ invoice/Controllers/InvoiceDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace invoice.Controllers
+{
+    public class InvoiceDetailController : Controller
+    {
+        // GET: InvoiceDetail
+        public ActionResult IndexInvoiceDetail()
+        {
+            return View();
+        }
+    }
+}
